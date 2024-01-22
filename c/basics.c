370d8eea9bf585890de011ad8b47d035ed623e70
jacelmoffett <jacelmoffett@gmail.com>
2024-01-22 15:41:09 -0500

    Added C file

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("This is my basics C program!\n");
+	int num;
+	printf("GIVE ME A NUMBER:  ");
+	scanf("%i", &num);
+	printf("You entered: %i \n", num);
+	return 0;
+}
